e8327857d9fe03c8804b3aa3a84eede42b7168e7
Fabiano Raiser <fabiano.raiser16@gmail.com>
2025-08-16 11:46:44 -0300

    Refatora o componente GraficoLinhas para aceitar dados dinâmicos e um rótulo personalizável
    - Modifica a função GraficoLinhas para receber um array de dados e um rótulo como parâmetros.
    - Remove dados estáticos do gráfico, permitindo maior flexibilidade na exibição de informações.

--- src/components/Lines/index.tsx
+++ src/components/Lines/index.tsx
@@ -2,18 +2,11 @@ import { LineChart } from "@mantine/charts";
 import { CustomCard } from "../Card";
 import { Title } from "@mantine/core";
 
-export const GraficoLinhas = () => {
-
-        const data = [
-            { month: "Janeiro", Vendas: 30 },
-            { month: "Fevereiro", Vendas: 50 },
-            { month: "Março", Vendas: 35 },
-            { month: "Abril", Vendas: 60 },
-        ]
+export const GraficoLinhas = (data: { period: string, value: number }[], label: string) => {
 
         return (
             <CustomCard>
-                <Title order={3}>Gráfico de Linhas</Title>
+                <Title order={3}>{label}</Title>
                 <LineChart 
                     h={300} 
                     data={data} 
